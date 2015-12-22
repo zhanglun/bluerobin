e072b1c0ab8b7de88bc0e2a7bb45e8d9051dfbfb
zhanglun <zhanglun1410@gmail.com>
2015-12-22 18:30:50 +0800

    init

---
+++ app/scripts/contentscript.js
@@ -0,0 +1,4 @@
+'use strict';
+
+console.log('\'Allo \'Allo! Content script');
+//# sourceMappingURL=contentscript.js.map
---
+++ app/scripts/options.js
@@ -0,0 +1,4 @@
+'use strict';
+
+console.log('\'Allo \'Allo! Option');
+//# sourceMappingURL=options.js.map
